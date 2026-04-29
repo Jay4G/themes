06bb15feb56692a8485041684e7ba479d03f4c59
Jay4G <143177827+Jay4G@users.noreply.github.com>
2026-04-29 13:00:14 +0100

    Code responsible for working with formidable forms

---
+++ twentytwentyfive/assets/js/geo.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    let countryElement = document.getElementById("geo-country");
+    if (!countryElement) return;
+
+    let country = countryElement.textContent.trim();
+
+    let inputField = document.querySelector("input[name='item_meta[123]']");
+    if (!inputField) return;
+
+    inputField.value = country;
+
+});
